dd1e22ed8118be946d5841c1a1c132b03fdaabd3
Chris <crystelratsimbazafy@gmail.com>
2020-09-14 10:46:57 +0300

    comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './assets/scss/style.scss';
+
+/**
+ * FONTAWESOME IS REQUIRED IN WEBSITE FOR BUILD MORE ICON
+ */
 
 require('@fortawesome/fontawesome-free/css/all.min.css');
 require('@fortawesome/fontawesome-free/css/fontawesome.min.css');
