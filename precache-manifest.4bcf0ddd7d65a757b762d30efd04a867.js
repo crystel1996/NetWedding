af8888532f35494402c2ac857e32f96ad3c784a5
Chris <crystelratsimbazafy@gmail.com>
2020-09-14 16:18:44 +0300

    Updates

---
+++ precache-manifest.4bcf0ddd7d65a757b762d30efd04a867.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d99eb9dd6cf545fdd69f3f431c283c",
+    "url": "/NetWedding/index.html"
+  },
+  {
+    "revision": "5f1756110a87ae9b56db",
+    "url": "/NetWedding/static/css/2.3936cf85.chunk.css"
+  },
+  {
+    "revision": "dee15a586fd542d2e695",
+    "url": "/NetWedding/static/css/main.03954fc8.chunk.css"
+  },
+  {
+    "revision": "5f1756110a87ae9b56db",
+    "url": "/NetWedding/static/js/2.23b7d881.chunk.js"
+  },
+  {
+    "revision": "1b9f3bf752a877bd1a04dbbaede1ef59",
+    "url": "/NetWedding/static/js/2.23b7d881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee15a586fd542d2e695",
+    "url": "/NetWedding/static/js/main.1937b6d9.chunk.js"
+  },
+  {
+    "revision": "82d5365551a3691bf2d3",
+    "url": "/NetWedding/static/js/runtime-main.dcff0cc8.js"
+  },
+  {
+    "revision": "c7edddd7ab9b179f765dc2130677d014",
+    "url": "/NetWedding/static/media/author-2.c7edddd7.jpg"
+  },
+  {
+    "revision": "4d141acc76e82f97aa6d13dec96b6fc1",
+    "url": "/NetWedding/static/media/author.4d141acc.jpg"
+  },
+  {
+    "revision": "4c013e2f7f4e266bd3b6312be153b9f1",
+    "url": "/NetWedding/static/media/banner1.4c013e2f.jpg"
+  },
+  {
+    "revision": "b9ced7c956625655fc82c6e809f74cfa",
+    "url": "/NetWedding/static/media/banner2.b9ced7c9.jpg"
+  },
+  {
+    "revision": "a4c46f70152ae621ce02e285cab8d1fd",
+    "url": "/NetWedding/static/media/banner3.a4c46f70.jpg"
+  },
+  {
+    "revision": "2cd3dcd36bb7c61c847ac770c2aff481",
+    "url": "/NetWedding/static/media/customer_1.2cd3dcd3.jpg"
+  },
+  {
+    "revision": "514eee1b06ddf4a5692f7264d3da79aa",
+    "url": "/NetWedding/static/media/customer_2.514eee1b.jpg"
+  },
+  {
+    "revision": "cfb95125014362aa10870999ff5d645d",
+    "url": "/NetWedding/static/media/customer_3.cfb95125.jpg"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/NetWedding/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/NetWedding/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/NetWedding/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/NetWedding/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/NetWedding/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/NetWedding/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/NetWedding/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/NetWedding/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/NetWedding/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/NetWedding/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/NetWedding/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/NetWedding/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/NetWedding/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/NetWedding/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/NetWedding/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "92d56f58d7ac0f5a27659040b6bb94bd",
+    "url": "/NetWedding/static/media/gallery_1.92d56f58.jpg"
+  },
+  {
+    "revision": "c0e0a5dff1c24181fbd44f4f8a086800",
+    "url": "/NetWedding/static/media/gallery_2.c0e0a5df.jpg"
+  },
+  {
+    "revision": "6efc24f718cc0236b65debdd3e74747b",
+    "url": "/NetWedding/static/media/gallery_3.6efc24f7.jpg"
+  },
+  {
+    "revision": "19494ce884781bcc137b2d6ef0a676a4",
+    "url": "/NetWedding/static/media/gallery_4.19494ce8.jpg"
+  },
+  {
+    "revision": "dd639e4f640bb0b16cfc0b17bc88d063",
+    "url": "/NetWedding/static/media/gallery_5.dd639e4f.jpg"
+  },
+  {
+    "revision": "3814d735f051b0d16fe90db705748fb2",
+    "url": "/NetWedding/static/media/gallery_6.3814d735.jpg"
+  }
+]);
